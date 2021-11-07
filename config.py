f818c21581d1266c8d05a9073f4f2959db449946
Percy <percy@railway.app>
2021-11-07 15:53:58 +0000

    Initial Commit

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "SUPERIOR_BOTS")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/7002282c03966f5f2360e.png")
+admins = {}
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "V3NOM_ASSISTANF")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "V3NOM_SUPPORT")
+PROJECT_NAME = getenv("PROJECT_NAME", "VenomMusic v5")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/itsunknown-12/Zaid-Vc-Player")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
